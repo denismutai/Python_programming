e986ae586a70fb6962665f5e0a98b778442d5dce
Denis Mutai <denismutai5@gmail.com>
2022-07-05 15:15:43 +0300

    read identifiers in python

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+print('Hello World')
+a=4
+b=5
+print(a+b)
+# use the print() function to print the line Hello, Python! to the screen. Use double quotes around the string;
+
+print ("Hello, Python!")
+
+# having done that, use the print() function again, but this time print your first name;
+
+print ("Denis Kibichiy Mutai")
+
+# remove the double quotes and run your code. Watch Python's reaction. What kind of error is thrown?
+ 
+#print( Denis Kibichiy )
+
+# then, remove the parentheses, put back the double quotes, and run your code again. What kind of error is thrown this time?
+
+#print "Denis Kibichiy"
+# experiment as much as you can. Change double quotes to single quotes, use multiple print() functions on the same line, and then on different lines. See what happens.
+
+print ('denis')
+print("Denis Kibichiy")
+print ("dENSIS")
