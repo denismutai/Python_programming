664092eedc2bfbb28a94dd54bd423f2212b3d765
Denis Mutai <denismutai5@gmail.com>
2022-07-11 06:20:33 +0300

    Learn python variables and data types

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Python_programming
-These repository is dedicated to code practices for intitial steps in learning python programming. 
-Contains exampls of variables, data types practices and quizzes.
-Designed to be my reference points in the future when I need to remind myself various subjects
+High-level, interpretted, 
+object oriented programming language-Makes use of objects (class $ attributes)
+Versatile programming language
+Dynamic programming language
+In python you can prompt the user for the input and assign it to a variable or use it immediately
+## Application 
+web application, GUI,Scientific and numeric, software development, data science
+## Variables
+__variables__ Name used to refer to memory allocation
+* __rules for declaring variables__
+a First character-lowercase alphabet or number
+b Name may not contain reserved keyword
+c May not contain special characters
+d The other letters of the variable name may contain a number, an underscore or   lowercase/uppercase character
+__Data types__ 
+int, float and string
+__operators__
+comparison,logical operators, arithmetic operators
--- hello_world.py
+++ hello_world.py
@@ -1,47 +1,59 @@
-print('Hello World')
-a=4
-b=5
-print(a+b)
-# use the print() function to print the line Hello, Python! to the screen. Use double quotes around the string;
+# print('Hello World')
+# a=4
+# b=5
+# print(a+b)
+# # use the print() function to print the line Hello, Python! to the screen. Use double quotes around the string;
 
-print ("Hello, Python!")
+# print ("Hello, Python!")
 
-# having done that, use the print() function again, but this time print your first name;
+# # having done that, use the print() function again, but this time print your first name;
 
-print ("Denis Kibichiy Mutai")
+# print ("Denis Kibichiy Mutai")
 
-# remove the double quotes and run your code. Watch Python's reaction. What kind of error is thrown?
+# # remove the double quotes and run your code. Watch Python's reaction. What kind of error is thrown?
  
-#print( Denis Kibichiy )
+# #print( Denis Kibichiy )
 
-# then, remove the parentheses, put back the double quotes, and run your code again. What kind of error is thrown this time?
+# # then, remove the parentheses, put back the double quotes, and run your code again. What kind of error is thrown this time?
 
-#print "Denis Kibichiy"
-# experiment as much as you can. Change double quotes to single quotes, use multiple print() functions on the same line, and then on different lines. See what happens.
-#mAKING USE OF THE PRINT FUNCTION
-print ('denis')
-print("Denis Kibichiy")
-print ("dENSIS")
+# #print "Denis Kibichiy"
+# # experiment as much as you can. Change double quotes to single quotes, use multiple print() functions on the same line, and then on different lines. See what happens.
+# #mAKING USE OF THE PRINT FUNCTION
+# print ('denis')
+# print("Denis Kibichiy")
+# print ("dENSIS")
 
-#MAKING IDENTIFIERS OR VARIABLES IN PYTHON
-#rules of variables- vaviables are case sensitive, variables can begin with alphabet or an underscore but may not begin with a special characters or numbers.  
-#Identifiers may not share a name with reserved keywords
-#all other characters except the first one maybe a uppercase or lowercase alphabet, a number, undescore or digit but may not have any whitespace or special character.
-#There is no requirement to declare variables in python, when we assign a value using (=) operator, the variable is declared automatically.
-# name_1="Denis"
-# name_2="Kibichiy"
-# country="Kenya"
-# currentYear=2022
-# birthYear=1998
-# welcomeMessage="I am a passionate software engineer from"
-# print(name_1 + '' + name_2)
+# #MAKING IDENTIFIERS OR VARIABLES IN PYTHON
+# #rules of variables- vaviables are case sensitive, variables can begin with alphabet or an underscore but may not begin with a special characters or numbers.  
+# #Identifiers may not share a name with reserved keywords
+# #all other characters except the first one maybe a uppercase or lowercase alphabet, a number, undescore or digit but may not have any whitespace or special character.
+# #There is no requirement to declare variables in python, when we assign a value using (=) operator, the variable is declared automatically.
+# # name_1="Denis"
+# # name_2="Kibichiy"
+# # country="Kenya"
+# # currentYear=2022
+# # birthYear=1998
+# # welcomeMessage="I am a passionate software engineer from"
+# # print(name_1 + '' + name_2)
 
-x=3
-y=4
-print(x+y)
-x=34+20
-print (10+x)
+# x=3
+# y=4
+# print(x+y)
+# x=34+20
+# print (10+x)
 
-#Dataty
+# #Datatypes
 
-#Object Reference- 
+# #Object Reference- 
+
+a=50
+b=60 
+print(a+b)
+a=500
+print (type(a))
+c=a+b
+print(c)
+y=input("Type a number:")
+x=input("Type a second number:")
+sum = int(x)+int(y)
+print(sum)
